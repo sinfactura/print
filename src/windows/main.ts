59b3d71a9325bc0475d730334a762ed0a2538421
Samuel Maison <toinmdq@gmail.com>
2023-12-11 13:45:39 +0100

    add first login view

--- src/windows/main.ts
+++ src/windows/main.ts
@@ -19,19 +19,37 @@ export const createMainWindow = () => {
 		backgroundColor: '#333',
 	});
 
+	let secondaryWindow = new BrowserWindow({
+		height: 350,
+		width: 800,
+		resizable: false,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+		show: false,
+		backgroundColor: '#333',
+		parent: mainWindow,
+		modal: true,
+		center: true,
+	});
+
 
 	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
 		mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+		secondaryWindow.loadFile('login.html');
 	} else {
 		mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+		secondaryWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/login.html`));
 	}
 
+
 	isDev && mainWindow.webContents.openDevTools();
+	isDev && secondaryWindow.webContents.openDevTools();
 
 	mainWindow.on('ready-to-show', mainWindow.show);
+	secondaryWindow.on('ready-to-show', secondaryWindow.show);
 
-	mainWindow.on('close', () => {
-		mainWindow = null;
-	});
+	mainWindow.on('close', () => mainWindow = null);
+	secondaryWindow.on('close', () => secondaryWindow = null);
 
 };
