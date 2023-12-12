2039d575a636d55f96a294bd011e45ca10e64843
Samuel Maison <toinmdq@gmail.com>
2023-12-12 09:36:20 +0100

    change indentation

--- forge.config.ts
+++ forge.config.ts
@@ -7,48 +7,48 @@ import { VitePlugin } from '@electron-forge/plugin-vite';
 // import path from 'node:path';
 
 const config: ForgeConfig = {
-  packagerConfig: {
-    // icon: path.join(__dirname, 'icons')
-    icon: './src/icons'
-  },
-  rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({
-      authors: 'SINFACTURA LLC',
-      description: 'Cloud print app for windows',
-      setupIcon: './src/icons/icon.ico'
-    }),
-    new MakerZIP({}, [ 'darwin', 'win32' ]),
-    new MakerRpm({}),
-    new MakerDeb({
-      options: {
-        icon: './src/icons/1024x1024.png'
-      }
-    })
-  ],
-  plugins: [
-    new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
-      build: [
-        {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'src/main.ts',
-          config: 'vite.main.config.ts',
-        },
-        {
-          entry: 'src/preload.ts',
-          config: 'vite.preload.config.ts',
-        },
-      ],
-      renderer: [
-        {
-          name: 'main_window',
-          config: 'vite.renderer.config.ts',
-        },
-      ],
-    }),
-  ],
+	packagerConfig: {
+		// icon: path.join(__dirname, 'icons')
+		icon: './src/icons'
+	},
+	rebuildConfig: {},
+	makers: [
+		new MakerSquirrel({
+			authors: 'SINFACTURA LLC',
+			description: 'Cloud print app for windows',
+			setupIcon: './src/icons/icon.ico'
+		}),
+		new MakerZIP({}, [ 'darwin', 'win32' ]),
+		new MakerRpm({}),
+		new MakerDeb({
+			options: {
+				icon: './src/icons/1024x1024.png'
+			}
+		})
+	],
+	plugins: [
+		new VitePlugin({
+			// `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+			// If you are familiar with Vite configuration, it will look really familiar.
+			build: [
+				{
+					// `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+					entry: 'src/main.ts',
+					config: 'vite.main.config.ts',
+				},
+				{
+					entry: 'src/preload.ts',
+					config: 'vite.preload.config.ts',
+				},
+			],
+			renderer: [
+				{
+					name: 'main_window',
+					config: 'vite.renderer.config.ts',
+				},
+			],
+		}),
+	],
 };
 
 export default config;
