7fb5ce3d765fb32e999d7990aa736b1b2b562839
Samuel Maison <toinmdq@gmail.com>
2023-12-14 10:15:40 +0100

    change makers

--- forge.config.ts
+++ forge.config.ts
@@ -1,27 +1,27 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import path from 'node:path';
 
 const config: ForgeConfig = {
 	packagerConfig: {
-		icon: path.join(__dirname, 'src', 'icons'),
-		extraResource: [
-			path.join(__dirname, 'src', 'icons')
-		]
+		icon: '/src/icons',
 	},
-	rebuildConfig: {},
 	makers: [
+		// windows distributable
 		new MakerSquirrel({
 			authors: 'SINFACTURA LLC',
 			description: 'Cloud print app',
+			setupIcon: path.join(__dirname, 'src', 'icons', 'icon.ico')
 		}),
+		// darwin and win32 ZIP file
 		new MakerZIP({}, [ 'darwin', 'win32' ]),
-		new MakerRpm({}),
-		new MakerDeb({})
+		// macOs distributable
+		new MakerDMG({
+			icon: path.join(__dirname, 'src', 'icons', 'icon.icns')
+		}),
 	],
 	plugins: [
 		new VitePlugin({
