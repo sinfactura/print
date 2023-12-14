4357dfb57ec71ff6874be0278ad3219e2f5c5307
Samuel Maison <toinmdq@gmail.com>
2023-12-14 10:16:12 +0100

    create some login logic

--- src/windows/main.ts
+++ src/windows/main.ts
@@ -4,47 +4,41 @@ import path from 'node:path';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+export let mainWindow: BrowserWindow;
 export const createMainWindow = async () => {
 
-	let mainWindow = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		height: 560,
 		width: isDev ? 1000 : 400,
-		// resizable: false,
-		icon: path.join(__dirname, 'icons', 'icon.png'),
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			nodeIntegration: true,
-			partition: 'persist:sinfactura'
 		},
 		show: false,
 		backgroundColor: '#333',
 	});
 
-	// const ses = mainWindow.webContents.session;
-	// await ses.cookies.set({ url: 'https://api.sinfactura.com', name: 'name', value: 'Samuel', expirationDate: 191212121 });
+	const ses = mainWindow.webContents.session;
+	await ses.cookies.set({ url: 'https://api.sinfactura.com', name: 'jwt', value: 'Samuel', expirationDate: 2191212121 });
 
-	// ses.cookies.get({ url: 'https://api.sinfactura.com' })
-	// 	.then((res) => console.log(res))
-	// 	.catch(err => console.log(err));
+	const cookieJwt = await ses.cookies.get({ url: 'https://api.sinfactura.com', name: 'jwt2' });
+	const isAuthenticated = cookieJwt.length > 0;
+
 
 	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-		mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-		// mainWindow.loadURL('https://sinfactura.com');
+		if (isAuthenticated) {
+			mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+		} else {
+			mainWindow.loadFile('login.html');
+		}
 	} else {
-		mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+		if (isAuthenticated) return mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+		mainWindow.loadFile('login.html');
 	}
 
 
 	isDev && mainWindow.webContents.openDevTools();
-
-	mainWindow.webContents.on('did-finish-load', () => {
-		console.log('finish-load');
-
-	});
-
-
 	mainWindow.on('ready-to-show', mainWindow.show);
-
 	mainWindow.on('close', () => mainWindow = null);
 
 };
