8b16fdac527182fb47547a5b91092b0104c802f3
Moises Samuel Maison Maison <toinmdq@gmail.com>
2024-10-23 22:46:32 +0200

    fix asar

--- forge.config.ts
+++ forge.config.ts
@@ -67,11 +67,11 @@ const config: ForgeConfig = {
 		new FusesPlugin({
 			version: FuseVersion.V1,
 			[ FuseV1Options.RunAsNode ]: false,
-			[ FuseV1Options.EnableCookieEncryption ]: true,
+			[ FuseV1Options.EnableCookieEncryption ]: false,
 			[ FuseV1Options.EnableNodeOptionsEnvironmentVariable ]: false,
 			[ FuseV1Options.EnableNodeCliInspectArguments ]: false,
-			[ FuseV1Options.EnableEmbeddedAsarIntegrityValidation ]: true,
-			[ FuseV1Options.OnlyLoadAppFromAsar ]: true,
+			[ FuseV1Options.EnableEmbeddedAsarIntegrityValidation ]: false,
+			[ FuseV1Options.OnlyLoadAppFromAsar ]: false,
 		}),
 	],
 };
